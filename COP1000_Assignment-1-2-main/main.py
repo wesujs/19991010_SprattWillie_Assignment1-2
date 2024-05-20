9e54a7544af5b6e6c9a0e5a5c707c759c433a86a
wesujs <93000934+wesujs@users.noreply.github.com>
2024-05-19 21:36:05 -0400

    Add files via upload

---
+++ COP1000_Assignment-1-2-main/main.py
@@ -0,0 +1,7 @@
+#A VARIABLE named user holds an INPUT enclosed in an INT() that the user can put in a number
+user = int(input("Enter a number: "))
+
+#A new VARIABLE named times_ten will multiply the user's INPUT by 10
+
+times_ten = user * 10
+print(times_ten)
